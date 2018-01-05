3a0410cab56f2e89fb3d3ac815c0237102529f6a
Michael Bohn <michael.bohn@freiheit.com>
2018-01-05 20:41:53 +0100

    select_arm impl + tests

--- src/softmax.rs
+++ src/softmax.rs
@@ -1,3 +1,4 @@
+extern crate rand;
 
 use super::bandit::{MultiArmedBandit};
 use std::collections::{HashMap};
@@ -6,19 +7,49 @@ use std::cmp::{Eq};
 
 pub struct AnnealingSoftmax<A: Hash + Eq> {
     pub arms: Vec<A>,
-    counts: HashMap<A, u32>
+    counts: HashMap<A, u64>,
+    values: HashMap<A, f64>
 }
 
-impl<A: Hash + Eq> AnnealingSoftmax<A> {
+impl<A: Clone + Hash + Eq> AnnealingSoftmax<A> {
     pub fn new(arms: Vec<A>) -> AnnealingSoftmax<A> {
-        return AnnealingSoftmax{arms, counts: HashMap::new()};
+        let mut values = HashMap::new();
+        for i in 0..arms.len() {
+            values.insert(arms[i].clone(), 0.0);
+        }
+        return AnnealingSoftmax::new_with_values(arms, values);
+    }
+
+    pub fn new_with_values(arms: Vec<A>, values: HashMap<A, f64>) -> AnnealingSoftmax<A> {
+        let mut counts = HashMap::new();
+        for i in 0..arms.len() {
+            counts.insert(arms[i].clone(), 0);
+        }
+        return AnnealingSoftmax{arms: arms, counts, values};
     }
 }
 
 impl<A: Clone + Hash + Eq> MultiArmedBandit<A> for AnnealingSoftmax<A> {
 
     fn select_arm(&self) -> A {
-        return self.arms[0].clone();
+
+        let temperature = 1.0;
+
+        let mut z = 0.0;
+        for v in self.values.values() {
+            z += (v / temperature).exp()
+        }
+
+        let rnd : f64 = rand::random();
+        let mut cum_prob = 0.0;
+        for (arm, v) in self.values.iter() {
+            let prob = ((v / temperature).exp() / z);
+            cum_prob += prob;
+            if cum_prob > rnd {
+                return arm.clone();
+            }
+        }
+        return self.arms[self.arms.len()-1].clone();
     }
 
     fn update(&self, arm: A, reward: f32) {
--- tests/softmax.rs
+++ tests/softmax.rs
@@ -3,18 +3,62 @@ extern crate bandit;
 
 use bandit::bandit::{MultiArmedBandit};
 use bandit::softmax::{AnnealingSoftmax};
+use std::collections::{HashMap};
 
-#[derive(Hash, PartialEq, Eq, Clone)]
-struct TestArm {
-    num: u32
-}
+const NUM_SELECTS : u32 = 100_000;
+const EPSILON : u32 = (NUM_SELECTS as f64 * 0.005) as u32;
 
 #[test]
 pub fn test_select_arm() {
     let arms = vec![TestArm{num: 0}, TestArm{num: 1}, TestArm{num: 2}, TestArm{num: 3}];
     let sm = AnnealingSoftmax::new(arms);
 
-    //TODO do some rounds of arm selection and check that arm is selected according to
-    // current probability (ignoring annealing + count = fixed for this test)
-    let arm_selected = sm.select_arm();
+    let mut selects : HashMap<TestArm, u32> = HashMap::new();
+    for _ in 0..NUM_SELECTS {
+        let arm_selected = sm.select_arm();
+        *selects.entry(arm_selected).or_insert(0) += 1;
+    }
+
+    let expected_count = abs_select(0.25);
+    for (arm, v) in selects {
+        assert_prop(expected_count, v, arm);
+    }
+}
+
+#[test]
+fn test_select_arm_with_given_values() {
+    let arms = vec![TestArm{num: 0}, TestArm{num: 1}, TestArm{num: 2}, TestArm{num: 3}];
+    let mut values = HashMap::new();
+    values.insert(arms[0].clone(), 0.2);
+    values.insert(arms[1].clone(), 0.2);
+    values.insert(arms[2].clone(), 0.1);
+    values.insert(arms[3].clone(), 0.5);
+    let sm = AnnealingSoftmax::new_with_values(arms.clone(), values);
+
+    let mut selects = HashMap::new();
+    for _ in 0..NUM_SELECTS {
+        let arm_selected = sm.select_arm();
+        *selects.entry(arm_selected).or_insert(0) += 1;
+    }
+
+    assert!(selects[&arms[3]] > selects[&arms[0]]);
+    assert!(selects[&arms[3]] > selects[&arms[1]]);
+
+    assert!(selects[&arms[0]] > selects[&arms[2]]);
+    assert!(selects[&arms[1]] > selects[&arms[2]]);
+}
+
+//Helper
+
+fn abs_select(prop: f64) -> u32 {
+    return (NUM_SELECTS as f64 * prop) as u32;
+}
+
+fn assert_prop(expected_count: u32, v: u32, arm: TestArm) {
+    assert!(expected_count - EPSILON < v && v < expected_count + EPSILON, "expected {}+-{}, got {} arm {:?}", expected_count, EPSILON, v, arm);
+}
+
+#[derive(Hash, PartialEq, Eq, Clone, Debug)]
+struct TestArm {
+    num: u32
 }
