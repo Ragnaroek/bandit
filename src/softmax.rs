5dfb0e1e26bc481fb87e7e947670fd88c5dd0ffd
Michael Bohn <michael.bohn@freiheit.com>
2018-02-15 06:49:49 +0100

    introduce cooldown_factor and full cooldown behaviour

--- src/softmax.rs
+++ src/softmax.rs
@@ -4,28 +4,43 @@ use super::bandit::{MultiArmedBandit};
 use std::collections::{HashMap};
 use std::hash::{Hash};
 use std::cmp::{Eq};
+use std;
+
+pub static DEFAULT_CONFIG : AnnealingSoftmaxConfig =  AnnealingSoftmaxConfig{cooldown_factor: 0.5};
+
+const E : f64 = 2.71828_18284_59045_23536;
 
 pub struct AnnealingSoftmax<A: Hash + Eq> {
+    config: AnnealingSoftmaxConfig,
     pub arms: Vec<A>,
     counts: HashMap<A, u64>,
     values: HashMap<A, f64>
 }
 
+#[derive(Copy, Clone)]
+pub struct AnnealingSoftmaxConfig {
+    /// The higher the value the faster the algorithms tends toward selecting
+    /// the arm with highest reward. Should be a number between [0, 1.0)
+    pub cooldown_factor : f64
+}
+
+
+
 impl<A: Clone + Hash + Eq> AnnealingSoftmax<A> {
-    pub fn new(arms: Vec<A>) -> AnnealingSoftmax<A> {
+    pub fn new(arms: Vec<A>, config: AnnealingSoftmaxConfig) -> AnnealingSoftmax<A> {
         let mut values = HashMap::new();
         for i in 0..arms.len() {
             values.insert(arms[i].clone(), 0.0);
         }
-        return AnnealingSoftmax::new_with_values(arms, values);
+        return AnnealingSoftmax::new_with_values(arms, config, values);
     }
 
-    pub fn new_with_values(arms: Vec<A>, values: HashMap<A, f64>) -> AnnealingSoftmax<A> {
+    pub fn new_with_values(arms: Vec<A>, config: AnnealingSoftmaxConfig, values: HashMap<A, f64>) -> AnnealingSoftmax<A> {
         let mut counts = HashMap::new();
         for i in 0..arms.len() {
             counts.insert(arms[i].clone(), 0);
         }
-        return AnnealingSoftmax{arms: arms, counts, values};
+        return AnnealingSoftmax{config, arms, counts, values};
     }
 }
 
@@ -33,17 +48,42 @@ impl<A: Clone + Hash + Eq> MultiArmedBandit<A> for AnnealingSoftmax<A> {
 
     fn select_arm(&self) -> A {
 
-        let temperature = 1.0;
+        let mut t : u64 = 1;
+        for v in self.counts.values() {
+            t += v;
+        }
+        let temperature = 1.0 / (t as f64 + 0.0000001).ln();
+        let cool_down = E*self.config.cooldown_factor;
 
-        let mut z = 0.0;
+        let mut z : f64 = 0.0;
         for v in self.values.values() {
-            z += (v / temperature).exp()
+            z += cool_down.powf(v / temperature)
+        }
+
+        if z.is_infinite() {
+            let mut highest_reward_arm : Option<&A> = None;
+            let mut highest_value = std::f64::MIN;
+            for (arm, v) in self.values.iter() {
+                if *v > highest_value {
+                    highest_value = *v;
+                    highest_reward_arm = Some(arm);
+                }
+            }
+            if highest_reward_arm.is_some() {
+                return highest_reward_arm.expect("highest reward arm").clone();
+            } else {
+                self.arms[self.arms.len()-1].clone();
+            }
         }
 
         let rnd : f64 = rand::random();
-        let mut cum_prob = 0.0;
+        let mut cum_prob : f64 = 0.0;
         for (arm, v) in self.values.iter() {
-            let prob = (v / temperature).exp() / z;
+            let mut prob = (cool_down.powf(v / temperature)) / z;
+
+            if prob.is_nan() {
+                prob = 0.0;
+            }
             cum_prob += prob;
             if cum_prob > rnd {
                 return arm.clone();
@@ -59,5 +99,6 @@ impl<A: Clone + Hash + Eq> MultiArmedBandit<A> for AnnealingSoftmax<A> {
 
         let val = self.values.entry(arm).or_insert(0.0);
         *val = ((n - 1.0) / n) * *val + (1.0 / n) * reward
+
     }
 }
--- tests/softmax.rs
+++ tests/softmax.rs
@@ -2,7 +2,7 @@
 extern crate bandit;
 
 use bandit::bandit::{MultiArmedBandit};
-use bandit::softmax::{AnnealingSoftmax};
+use bandit::softmax::{AnnealingSoftmax, AnnealingSoftmaxConfig, DEFAULT_CONFIG};
 use std::collections::{HashMap};
 
 const NUM_SELECTS : u32 = 100_000;
@@ -11,7 +11,7 @@ const EPSILON : u32 = (NUM_SELECTS as f64 * 0.005) as u32;
 #[test]
 pub fn test_select_arm() {
     let arms = vec![TestArm{num: 0}, TestArm{num: 1}, TestArm{num: 2}, TestArm{num: 3}];
-    let sm = AnnealingSoftmax::new(arms);
+    let sm = AnnealingSoftmax::new(arms, DEFAULT_CONFIG);
 
     let mut selects : HashMap<TestArm, u32> = HashMap::new();
     for _ in 0..NUM_SELECTS {
@@ -26,26 +26,53 @@ pub fn test_select_arm() {
 }
 
 #[test]
-fn test_select_arm_with_given_values() {
+fn test_moves_towards_arm_with_highest_reward_with_high_cooldown() {
     let arms = vec![TestArm{num: 0}, TestArm{num: 1}, TestArm{num: 2}, TestArm{num: 3}];
-    let mut values = HashMap::new();
-    values.insert(arms[0].clone(), 0.2);
-    values.insert(arms[1].clone(), 0.2);
-    values.insert(arms[2].clone(), 0.1);
-    values.insert(arms[3].clone(), 0.5);
-    let sm = AnnealingSoftmax::new_with_values(arms.clone(), values);
+    let arm_test_rewards = vec![98.0, 100.0, 99.0, 98.5];
+    let mut sm = AnnealingSoftmax::new(arms.clone(), AnnealingSoftmaxConfig{cooldown_factor: 0.88});
 
-    let mut selects = HashMap::new();
-    for _ in 0..NUM_SELECTS {
-        let arm_selected = sm.select_arm();
-        *selects.entry(arm_selected).or_insert(0) += 1;
+    let num_iterations = 500;
+
+    let mut selects = Vec::<[u64;4]>::with_capacity(num_iterations);
+    for _ in 0..num_iterations {
+        for i in 0..arms.len() {
+            sm.update(arms[i], arm_test_rewards[i])
+        }
+
+        let mut draws = [0;4];
+        for _ in 0..1000 {
+            let selected_arm = sm.select_arm();
+            draws[selected_arm.num as usize] += 1;
+        }
+        selects.push(draws);
     }
 
-    assert!(selects[&arms[3]] > selects[&arms[0]]);
-    assert!(selects[&arms[3]] > selects[&arms[1]]);
+    assert!(selects[num_iterations-1][1] >= 998, format!("last round should favour highest reward, but did not {}", selects[num_iterations-1][1]));
+}
 
-    assert!(selects[&arms[0]] > selects[&arms[2]]);
-    assert!(selects[&arms[1]] > selects[&arms[2]]);
+#[test]
+fn test_always_selects_highest_reward_if_totally_cooled_down() {
+    let arms = vec![TestArm{num: 0}, TestArm{num: 1}, TestArm{num: 2}, TestArm{num: 3}];
+    let arm_test_rewards = vec![98.0, 100.0, 99.0, 98.5];
+    let mut sm = AnnealingSoftmax::new(arms.clone(), AnnealingSoftmaxConfig{cooldown_factor: 1.0});
+
+    let num_iterations = 1000;
+
+    let mut selects = Vec::<[u64;4]>::with_capacity(num_iterations);
+    for _ in 0..num_iterations {
+        for i in 0..arms.len() {
+            sm.update(arms[i], arm_test_rewards[i])
+        }
+
+        let mut draws = [0;4];
+        for _ in 0..1000 {
+            let selected_arm = sm.select_arm();
+            draws[selected_arm.num as usize] += 1;
+        }
+        selects.push(draws);
+    }
+
+    assert_eq!(selects[num_iterations-1][1], 1000);
 }
 
 //Helper
@@ -58,7 +85,7 @@ fn assert_prop(expected_count: u32, v: u32, arm: TestArm) {
     assert!(expected_count - EPSILON < v && v < expected_count + EPSILON, "expected {}+-{}, got {} arm {:?}", expected_count, EPSILON, v, arm);
 }
 
-#[derive(Hash, PartialEq, Eq, Clone, Debug)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
 struct TestArm {
     num: u32
 }
